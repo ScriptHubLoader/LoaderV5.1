39e024881c686a6c71b5e3daa32607ba326f575e
ScriptHubLoader <infuraswapzoraswap2@gmail.com>
2025-07-19 18:38:37 +0800

    Update MainV1.lua

--- MainV1.lua
+++ MainV1.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/4iVxjO7tFx/0", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/er/refs/heads/main/er1"))()
