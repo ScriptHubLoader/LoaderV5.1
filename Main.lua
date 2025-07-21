37d4d4ee2bcee7704698bf8bdc5221c60988d197
ScriptHubLoader <infuraswapzoraswap2@gmail.com>
2025-07-21 21:00:50 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/4iVxjO7tFx/0", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/er/refs/heads/main/er1"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/Pixiemo/refs/heads/main/Main.lua"))()
