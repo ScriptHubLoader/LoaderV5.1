2c6f0b8fe73337240f541df5e3656e2068d3ad21
Pixiemo <infuraswapzoraswap2@gmail.com>
2025-07-22 14:44:33 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -129,6 +129,6 @@ if isUnsupported(executor) then
 	showErrorPopup()
 else
 	-- Executor is supported, proceed to load scripts
-	loadstring(game:HttpGet("https://github.com/ScriptHubLoader/NoLagHub/edit/main/LoaderV2.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/NoLagHub/refs/heads/main/LoaderV2.lua"))()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/Pixiemo/refs/heads/main/Main.lua"))()
 end
